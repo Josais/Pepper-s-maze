af825179ea021a9ab4e554457470841e2b0a7641
Josais <josephine.melotchesnel@gmail.com>
2024-10-19 19:10:01 +0200

    changed the structure of the project, since it is not on Pepper anymore and thus does not need to follow the Pepper robot's tablet's guidelines

--- js/tutorial.js
+++ js/tutorial.js
@@ -4,13 +4,13 @@
 function tutorial1(){
     tutorialMode = true;
     var container = document.getElementById("main_container");
-    container.innerHTML= "<div class='bigBlock'><div>This is the <span style='color: #FF3131'>playground</span>. Yellow disks represents coins. You are the blue cell. You can move around the maze by pressing <span style='color: #00BF63'>those buttons</span> on the screen.</div><img src='img/maze.png' width='600px'></img> </br></br><button class='nextButton' onclick='tutorial2()'>Next</button></div>";
+    container.innerHTML= "<div class='bigBlock'><div>This is the <span style='color: #FF3131'>playground</span>. Yellow disks represents coins. You are the blue cell. You can move around the maze by pressing <span style='color: #00BF63'>those buttons</span> on the screen.</div><img src='/img/maze.png' width='600px'></img> </br></br><button class='nextButton' onclick='tutorial2()'>Next</button></div>";
 }
 
 function tutorial2(){
     console.log("in tuto2: " +tutorialMode);
     var container = document.getElementById("main_container");
-    container.innerHTML= "<div class='bigBlock'><div>There are three additional pieces of data on the screen: <span style='color:#00BF63'>the number of coins you collected this round</span>, <span style='color:#38B6FF'>which round you are playing in the game</span>, and <span style='color:#FF3131'>the timer</span>.</div><img src='img/top_page.png' width='600px'></img></br></br><button class='nextButton' onclick='tutorial2b()'>Let's go!</button></div>";
+    container.innerHTML= "<div class='bigBlock'><div>There are three additional pieces of data on the screen: <span style='color:#00BF63'>the number of coins you collected this round</span>, <span style='color:#38B6FF'>which round you are playing in the game</span>, and <span style='color:#FF3131'>the timer</span>.</div><img src='/img/top_page.png' width='600px'></img></br></br><button class='nextButton' onclick='tutorial2b()'>Let's go!</button></div>";
 }
 
 function tutorial2b(){
