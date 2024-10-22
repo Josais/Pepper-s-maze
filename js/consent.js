694c8946f513d6d794a08bcf7fdc8d58cb7207cd
Josais <josephine.melotchesnel@gmail.com>
2024-10-22 19:56:09 +0200

    All of the css of the consent form is done

--- js/consent.js
+++ js/consent.js
@@ -1,4 +1,4 @@
-const consentForm = "Please read the statements below. </br></br>- I confirm that I am 18 years of age or older.</br> - I confirm that the research project on communication in collaborative human-robot teams has been explained to me. I have had the opportunity to ask questions about the project and have had theses answered satisfactorily. I had enough time to consider whether to participate.</br> - I consent to the material I contribute being used to generate insights for the research project.</br> - I understand that all data will be stored anonymously and cannot be traced back to me personally.</br>- I understand that my participation in this research is voluntary and that I can withdraw consent at any point before and during the experiment without providing a reason, and that if I withdraw before the end of the experiment, any collected data already collected from me will be erased. I understand that I cannot ask for my responses to be erased after the end of the experiment, as they will be anonymously stored and thus cannot be traced back to me.</br>- I consent to the fully anonymized data to be used in future publications and other scholarly means of disseminating the findings from the research project. I understand that the data acquired will be securely stored by researchers, but that appropriately anonymized data may in future be made available to others for research purposes. I understand that the University may publish appropriately anonymized data in appropriate data repositories for verification purposes and to make it accessible to researchers and other research users.</br></br>";
+const consentForm = "</br>Please read the statements below. </br></br>- I confirm that I am 18 years of age or older.</br>- I confirm that the research project on communication in collaborative human-robot teams has been explained to me. I have had the opportunity to ask questions about the project and have had theses answered satisfactorily. I had enough time to consider whether to participate.</br>- I consent to the material I contribute being used to generate insights for the research project.</br>- I understand that all data will be stored anonymously and cannot be traced back to me personally.</br>- I understand that my participation in this research is voluntary and that I can withdraw consent at any point before and during the experiment without providing a reason, and that if I withdraw before the end of the experiment, any collected data already collected from me will be erased. I understand that I cannot ask for my responses to be erased after the end of the experiment, as they will be anonymously stored and thus cannot be traced back to me.</br>- I consent to the fully anonymized data to be used in future publications and other scholarly means of disseminating the findings from the research project. I understand that the data acquired will be securely stored by researchers, but that appropriately anonymized data may in future be made available to others for research purposes. I understand that the University may publish appropriately anonymized data in appropriate data repositories for verification purposes and to make it accessible to researchers and other research users.</br>";
 
 function showConsentForm(){
     var container = document.getElementById("main_container");
@@ -10,11 +10,13 @@ function showConsentForm(){
     form.style.color = "white";
     form.innerHTML = consentForm;
     form.style.width = "60%";
+    form.className = "row2";
 
     container.appendChild(form);
 
     var buttonsconsent = document.createElement("div");
     buttonsconsent.innerHTML = `<button class="consent_button" onclick="consent()">I consent to all of the above</button><button class="consent_button" onclick="dontconsent()">I do not consent</button>`;
+    buttonsconsent.className = "row3";
 
     container.appendChild(buttonsconsent);
 }
